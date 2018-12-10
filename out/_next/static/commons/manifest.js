7f6027906ef00b4ce34236f466c1809736d78e82
mattdamon108 <woonki.moon@gmail.com>
2018-12-10 10:44:32 +0900

    Deploy Next.js

--- out/_next/static/commons/manifest.js
+++ out/_next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "08889aa15102d608fcc8"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "24827ac2cdb61e9b50a0"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"chunks/posts_2018_09_27_postmaster_pid_Location_md_08349d0ce88dae8d32256ba7f1738d6f","1":"chunks/posts_2018_09_08_How_to_set_state_with_data_from_graphQL_query_md_3d1727a05d42d2cd617a2de552d8adf7","2":"chunks/posts_2018_09_01_How_to_highlight_code_in_jekyll_md_c5a6c4ffd28351d0b737ad242d8eae65","3":"chunks/posts_2018_08_31_Nested_Comment_Reply_sturcture_with_ReactJS_md_850b839f0ab2b756659a1fbe93ccb9c7","4":"chunks/posts_2018_08_30_How_to_align_element_to_vertical_center_md_16407fd3bc50200b5ad5ab9c17ecb507","5":"chunks/posts_2018_08_28_File_upload_with_GraphQL_Apollo_Client_Django_Graphene_md_3c2d1280e6e24e9e33c5fb09df3ee21f","6":"chunks/posts_2018_08_25_Making_virtualenv_for_Django_md_3ba9ecf7b150de600b58727bd15bb216","7":"chunks/posts_2018_08_24_Markdown_summary_md_6a9997ee299d9c51986a2a4070662061","8":"chunks/posts_2018_08_24_First_Post_md_4b4a2caaefc36da171c0b8efc5709dc2","9":"main.js","10":"bundles/pages/_app.js","11":"bundles/pages/_document.js","12":"bundles/pages/_error.js","14":"bundles/pages/index.js","15":"bundles/pages/post.js"}[chunkId]||chunkId) + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"chunks/posts_2018_09_27_postmaster_pid_Location_md_08349d0ce88dae8d32256ba7f1738d6f","1":"chunks/posts_2018_09_08_How_to_set_state_with_data_from_graphQL_query_md_3d1727a05d42d2cd617a2de552d8adf7","2":"chunks/posts_2018_09_01_How_to_highlight_code_in_jekyll_md_c5a6c4ffd28351d0b737ad242d8eae65","3":"chunks/posts_2018_08_31_Nested_Comment_Reply_sturcture_with_ReactJS_md_850b839f0ab2b756659a1fbe93ccb9c7","4":"chunks/posts_2018_08_30_How_to_align_element_to_vertical_center_md_16407fd3bc50200b5ad5ab9c17ecb507","5":"chunks/posts_2018_08_28_File_upload_with_GraphQL_Apollo_Client_Django_Graphene_md_3c2d1280e6e24e9e33c5fb09df3ee21f","6":"chunks/posts_2018_08_25_Making_virtualenv_for_Django_md_3ba9ecf7b150de600b58727bd15bb216","7":"chunks/posts_2018_08_24_Markdown_summary_md_6a9997ee299d9c51986a2a4070662061","8":"chunks/posts_2018_08_24_First_Post_md_4b4a2caaefc36da171c0b8efc5709dc2","9":"main.js","10":"bundles/pages/_app.js","11":"bundles/pages/_document.js","12":"bundles/pages/_error.js","14":"bundles/pages/post.js","15":"bundles/pages/index.js"}[chunkId]||chunkId) + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
