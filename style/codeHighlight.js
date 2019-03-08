4da2661405a8d1998bc12b044233b0103e3ba775
moondaddi <woonki.moon@gmail.com>
2019-03-08 15:31:18 +0900

    change image fit to content

--- style/codeHighlight.js
+++ style/codeHighlight.js
@@ -25,6 +25,11 @@ const codeHighlighting = `
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 
+img {
+	width: 100%;
+	max-width: fit-content;
+}
+
 blockquote {
   margin: 0;
   padding: 0 1em;
