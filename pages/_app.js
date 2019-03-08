deb077e5146a05cc296d23bf07be1a5c04165095
moondaddi <woonki.moon@gmail.com>
2019-03-08 18:05:53 +0900

    fix scroll event

--- pages/_app.js
+++ pages/_app.js
@@ -1,62 +1,62 @@
-import App, { Container } from "next/app";
-import React from "react";
-import Header from "../components/Header";
-import Category from "../components/Category";
-import Footer from "../components/Footer";
-import cssGlobal from "../style/cssGlobal";
-import Head from "next/head";
+import App, { Container } from "next/app"
+import React from "react"
+import Header from "../components/Header"
+import Category from "../components/Category"
+import Footer from "../components/Footer"
+import cssGlobal from "../style/cssGlobal"
+import Head from "next/head"
 
 class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {};
+    let pageProps = {}
 
     if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
+      pageProps = await Component.getInitialProps(ctx)
     }
-    return { pageProps };
+    return { pageProps }
   }
   state = {
     isShowing: null
-  };
+  }
   componentDidMount() {
-    const header = document.querySelector("#header");
-    const container = document.querySelector(".container");
-    const containerCategory = document.querySelector("#container-category");
+    const header = document.querySelector("#header")
+    const container = document.querySelector(".container")
+    const containerCategory = document.querySelector("#container-category")
     window.addEventListener("scroll", () => {
-      if (window.scrollY > 75) {
-        header.classList.add("make-fixed");
-        container.classList.add("add-padding-for-fixed-header");
-        containerCategory.classList.add("make-fixed-category");
+      if (window.pageYOffset > header.offsetTop + 250) {
+        header.classList.add("make-fixed")
+        container.classList.add("add-padding-for-fixed-header")
+        containerCategory.classList.add("make-fixed-category")
       } else {
-        header.classList.remove("make-fixed");
-        container.classList.remove("add-padding-for-fixed-header");
-        containerCategory.classList.remove("make-fixed-category");
+        header.classList.remove("make-fixed")
+        container.classList.remove("add-padding-for-fixed-header")
+        containerCategory.classList.remove("make-fixed-category")
       }
-    });
+    })
   }
   componentWillUnmount() {
-    window.removeEventListener("scroll");
+    window.removeEventListener("scroll")
   }
   _setShowing = name => {
     if (name) {
       this.setState({
         isShowing: name
-      });
+      })
     } else {
       if (this.state.isShowing) {
         this.setState({
           isShowing: null
-        });
+        })
       } else {
         this.setState({
           isShowing: "category"
-        });
+        })
       }
     }
-  };
+  }
   render() {
-    const { Component, pageProps, router } = this.props;
-    const { isShowing } = this.state;
+    const { Component, pageProps, router } = this.props
+    const { isShowing } = this.state
 
     return (
       <Container>
@@ -77,8 +77,8 @@ class MyApp extends App {
           {cssGlobal}
         </style>
       </Container>
-    );
+    )
   }
 }
 
-export default MyApp;
+export default MyApp
